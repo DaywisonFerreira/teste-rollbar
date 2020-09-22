da40801a9350aad89b27eb17578be1b0f5da5bb4
Daywison Leal <daywison.leal@gmail.com>
2020-09-22 17:01:46 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const Rollbar = require('rollbar');
+const rollbar = new Rollbar('e7ac80802aec438aa6fe09bf4e6ec6b0');
+
+const app = express();
+
+app.get('/', function(req, res) {
+  const foo = {
+    hello: "world"
+  }
+
+  console.log(foo.bar())
+});
+
+app.use(rollbar.errorHandler());
+
+app.listen(9999);
